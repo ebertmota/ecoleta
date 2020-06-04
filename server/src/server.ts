0c22c5d1d0b6add98a7c1c7f5cfc4e6e2d01545a
ebertmota <ebert.mota03@gmail.com>
2020-06-03 21:16:55 -0300

    setting server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import { resolve } from 'path';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('Something here');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
-
-  res.json({"message": 'Hello Guys'})
-})
-
-app.listen(3333)
+app.listen(3333);
