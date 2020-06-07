c7f7bf1059b7e3b46f3378efb17e3a3c6e3e6c0c
ebertmota <ebert.mota03@gmail.com>
2020-06-06 21:51:39 -0300

    adding ToastContainer for toastify notifications

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
@@ -8,6 +8,7 @@ function App() {
     <>
       <Routes />
       <GlobalStyles />
+      <ToastContainer autoClose={3000} />
     </>
 
   );
