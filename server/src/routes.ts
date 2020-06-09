d87ec72b61e91318647335e9814d0e3285617c2e
ebertmota <ebert.mota03@gmail.com>
2020-06-08 21:22:30 -0300

    using celebrate validation in middleware

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import multer from 'multer';
-import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
 
+import { checkBody } from './validators/pointValidator';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -22,20 +22,7 @@ routes.get('/points/:id', pointsController.show);
 routes.post(
   '/points',
   upload.single('image'),
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required(),
-      email: Joi.string().required().email(),
-      whatsapp: Joi.number().required(),
-      latitude: Joi.number().required(),
-      longitude: Joi.number().required(),
-      city: Joi.string().required(),
-      uf: Joi.string().required().max(2),
-      items: Joi.string().required(),
-    }),
-  }, {
-    abortEarly: false,
-  }),
+  checkBody,
   pointsController.create,
 );
 
