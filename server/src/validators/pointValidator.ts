42062a0202b870cf88938df3cbd70102d216639e
ebertmota <ebert.mota03@gmail.com>
2020-06-08 21:20:41 -0300

    celebrate validators

---
+++ server/src/validators/pointValidator.ts
@@ -0,0 +1,16 @@
+import { celebrate, Segments, Joi } from 'celebrate';
+
+export const checkBody = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().required().max(2),
+    items: Joi.string().required(),
+  }),
+}, {
+  abortEarly: false,
+});
